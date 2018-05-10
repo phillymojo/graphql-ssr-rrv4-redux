a586e3faca5b7929a1b9a3f3dc86b006fd8e0092
morgan.bonar <morgan.bonar@nike.com>
2018-05-10 13:27:22 -0700

    removing bundle.js from repo, adding structure to pw html

--- src/components/pw.js
+++ src/components/pw.js
@@ -1,47 +1,49 @@
 import React from "react"
 import { connect } from "react-redux";
 import Header from './header';
- 
+
 class PW extends React.Component {
 
   render() {
     const analyzer = this.props.pw.analyzer;
     return (
-      <div>
+      <div className="pw">
         <Header />
         <br />
-        { (this.props.isLoading) ? <div>Loading...</div> : 
-        <div>
-          <div>The request for {analyzer.url} should redirect to {analyzer.redirectUrl}</div>
-          <ul>
-            {
-              this.props.pw.navlinks.map((navlink, i) => {
-                return <li key={i}>{navlink.displayText}</li>
-              })
-            }
-          </ul>
-          <ul>
-            {
-              this.props.pw.products.map((product, i) => {
-                return <li key={i}>{product.title}</li>
-              })
-            }
-          </ul>
-        </div>
+        {(this.props.isLoading) ? <div className="isLoading">Loading...</div> :
+          <div>
+            <div className="redirect">The request for {analyzer.url} should redirect to {analyzer.redirectUrl}</div>
+            <div className="content">
+              <ul className="navlinks">
+                {
+                  this.props.pw.navlinks.map((navlink, i) => {
+                    return <li key={i}>{navlink.displayText}</li>
+                  })
+                }
+              </ul>
+              <ul className="products">
+                {
+                  this.props.pw.products.map((product, i) => {
+                    return <li key={i}>{product.title}</li>
+                  })
+                }
+              </ul>
+            </div>
+          </div>
         }
       </div>
     )
   }
 }
 
-const mapStateToProps = (state) => { 
- return {
-  //  newsItems: state.newsItems.data.news,
-  pw: state.PW.data.pw,
-  isLoading: state.isLoading
- }
+const mapStateToProps = (state) => {
+  return {
+    //  newsItems: state.newsItems.data.news,
+    pw: state.PW.data.pw,
+    isLoading: state.isLoading
+  }
 }
- 
+
 export const PWConnected = connect(
   mapStateToProps
 )(PW)
